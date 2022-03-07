38337cd6cfc5bd69c11425f16cdfa4c67bfde357
Viaan22 <89401601+Viaan22@users.noreply.github.com>
2022-03-07 20:23:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function addUser(){
+    user_name=document.getElementById("user_name").value;
+    localStorage.setItem("user_name",user_name);
+    window.location="kwitter_room.html";
+  <h4>The account will be created soon You will be able to see it in 24 hours</h4>
+}
